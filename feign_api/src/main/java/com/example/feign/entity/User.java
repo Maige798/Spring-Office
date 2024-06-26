3c0380ff299c7fa8b61108018ace9ba434ee586e
Maige798 <3317991292@qq.com>
2024-06-26 09:52:12 +0800

    创建feign远程调用模块

---
+++ feign_api/src/main/java/com/example/feign/entity/User.java
@@ -0,0 +1,31 @@
+package com.example.feign.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+    private Integer id;
+    private String name;
+    private Integer isAdmin;
+    private String photo;
+    private Integer sex;
+    private String phone;
+    private String email;
+    @JsonProperty("hire_date")
+    private Date hireDate;
+    @JsonProperty("create_time")
+    private Timestamp createTime;
+    private Integer status;
+    @JsonProperty("role_id")
+    private Integer roleId;
+    @JsonProperty("dept_id")
+    private Integer deptId;
+}
