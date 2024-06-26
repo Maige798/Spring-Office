d23517e8e76c6a7151c20741d885f94dc2d8771d
Maige798 <3317991292@qq.com>
2024-06-26 19:56:18 +0800

    修改了角色管理的接口定义，现在创建角色不需要依赖于用户管理的服务

--- role-service/src/main/java/com/springoffice/role/RoleServiceApplication.java
+++ role-service/src/main/java/com/springoffice/role/RoleServiceApplication.java
@@ -2,8 +2,10 @@ package com.springoffice.role;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.springoffice")
 public class RoleServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RoleServiceApplication.class, args);
