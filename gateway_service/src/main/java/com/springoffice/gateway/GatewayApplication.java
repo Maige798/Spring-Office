a0c395bd5c2f7160694d10c0686faf0b15b5027e
Maige798 <3317991292@qq.com>
2024-06-26 14:22:21 +0800

    新增网关模块，优化maven依赖

---
+++ gateway_service/src/main/java/com/springoffice/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.springoffice.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.springoffice")
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
