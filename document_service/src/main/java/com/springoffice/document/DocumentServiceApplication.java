71f15de400441077adf363387521f83bfd8e3002
Maige798 <3317991292@qq.com>
2024-07-02 09:38:48 +0800

    创建文档服务模块

---
+++ document_service/src/main/java/com/springoffice/document/DocumentServiceApplication.java
@@ -0,0 +1,15 @@
+package com.springoffice.document;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableFeignClients
+@ComponentScan("com.springoffice")
+public class DocumentServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentServiceApplication.class, args);
+    }
+}
